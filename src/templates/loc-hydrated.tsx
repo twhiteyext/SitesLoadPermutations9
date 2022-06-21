38d4c1d93d243275c771ad3c3713a99fbcbde207
twhiteyext <87829666+twhiteyext@users.noreply.github.com>
2022-06-21 14:13:28 -0400

    Update loc-hydrated.tsx

--- src/templates/loc-hydrated.tsx
+++ src/templates/loc-hydrated.tsx
@@ -5,9 +5,9 @@ import { reactWrapper } from "../wrapper";
 
 export const config: FeatureConfig = {
   name: "loc-hydrated",
-  streamId: "sites-load-5-100-1000000",
+  streamId: "sites-load-5-100-1000000-9",
   stream: {
-    "$id": "sites-load-5-100-1000000",
+    "$id": "sites-load-5-100-1000000-9",
     "source": "knowledgeGraph",
     "destination": "pages",
     "fields": [
